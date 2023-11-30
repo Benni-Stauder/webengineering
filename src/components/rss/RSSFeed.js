46182be29426d99d872a2b91f00e8667b34a5f7f
Benni Stauder <benni@bennistauder.de>
2023-11-30 15:59:13 +0100

    Pain

--- src/components/rss/RSSFeed.js
+++ src/components/rss/RSSFeed.js
@@ -1,31 +1,68 @@
-import {Component} from "react";
+import React, {Component} from 'react';
 
-const tagesschau = "https://www.tagesschau.de/infoservices/alle-meldungen-100~rss2.xml";
-const RTL = "https://www.rtl.de/rss/feed/news";
+function getSrc(content) {
+    let splitContent = content.split('"');
+    return splitContent[1]
+}
 
-class RSSFeed extends Component {
+function getHeight(content) {
+    let splitContent = content.split('"');
+    return splitContent[5]
+}
+
+function getWidth(content) {
+    let splitContent = content.split('"');
+    return splitContent[3]
+}
+
+function getAlign(content) {
+    let splitContent = content.split('"');
+    return splitContent[7]
+}
+
+export class RSSFeed extends Component{
     constructor(props) {
         super(props);
+
+        this.state = {
+            rss: []
+        };
+        this.setRss = this.setRss.bind(this);
+    }
+    setRss(json) {
         this.setState({
-            data: []
-        })
+            rss: json.items
+        });
+    }
+    render () {
+        console.log(this.state.rss.map)
+        const listItems = this.state.rss.map(items =>
+            <li key={items.guid}>
+                <img src={getSrc(items.content)} alt={items.titel} height={getHeight(items.content)} width={getWidth(items.content)} align={getAlign(items.content)}/>
+                <p>
+                    <b>
+                        <a href = {items.link}>{items.title}</a>
+                    </b>
+                </p>
+            </li>
+        );
+        return <ul>{listItems}</ul>;
     }
 
     componentDidMount() {
-        const qryRSS = 'select * from rss where url='+'"'+RTL+'"';
-        fetch("http://query.yahooapis.com/v1/public/yql", {q: qryRSS, format: "json"}).then(
-            (data) => {
-                console.log(data)
-            }
-        )
-    }
-    render() {
-        return (
-            <div>
-
-            </div>
-        )
+        const parseUrl = 'https://api.rss2json.com/v1/api.json?rss_url=';
+        const rssUrl = 'https://www.rtl.de/rss/feed/news';
+        fetch(parseUrl + rssUrl)
+            .then(response => response.json())
+            .then((json) => {
+                if (json.status === 'ok') {
+                    this.setRss(json);
+                    console.log(json)
+                }else {
+                    console.log("failed");
+                }
+            });
     }
 }
 
-export default RSSFeed;
+export default RSSFeed
