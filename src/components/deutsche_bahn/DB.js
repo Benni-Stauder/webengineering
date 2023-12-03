a9484fab664a5536412b7a05a466cdc22b2ae279
Benni Stauder <benni@bennistauder.de>
2023-12-03 20:14:50 +0100

    Pain

--- src/components/deutsche_bahn/DB.js
+++ src/components/deutsche_bahn/DB.js
@@ -1,7 +1,8 @@
-import {useEffect, useState} from "react";
+import {useState} from "react";
 import {buildIndex} from "./synchronous_autocomplete";
 import normalize from 'normalize-for-search'
 import {createAutocomplete} from "./autocomplete";
+import {dbcache} from "./data/cache";
 
 const fetchFragment = async (offset) => {
     return fetch(`https://apis.deutschebahn.com/db-api-marketplace/apis/ris-stations/v1/stations?offset=${offset}&limit=100`, {
@@ -29,10 +30,16 @@ const tokenize = str => normalize(str).replace(/[^\w\s]/g, '').split(/\s+/g)
 
 const DB = () => {
     const [data, setData] = useState([])
+    const [searchWord, setSearchWord] = useState([])
+    const [suggestion, setSuggestion] = useState([])
 
-    useEffect(() => {
-        fetchData()
-    }, []);
+    const applicationStyle = {}
+    const containerStyle = {width: '280px', display: 'flex', flexDirection: 'column'}
+    const dropDownStyle = {backgroundColor: 'white', display: 'flex', flexDirection: 'column', border: '1px solid gray'}
+    const dropDownEmptyStyle = {border: 'none'}
+    const dropDownRowStyle = {cursor: 'pointer', textAlign: 'start', margin: '2px 0'}
+    const inputStyle = {width: '220px'}
+    const searchInnerStyle = {display: 'flex'}
 
     const fetchData = async () => {
         let offset = 0
@@ -48,15 +55,61 @@ const DB = () => {
         setData(tmp)
     }
 
-    const index = buildIndex(tokenize, data)
+    let names = []
+    dbcache.forEach(item => names.push(
+        {
+            'id': item.stationID,
+            'name': item.names.DE.name,
+            'weight': 1
+        })
+    )
+    const index = buildIndex(tokenize, names)
     const autocomplete = createAutocomplete(index, tokenize)
 
-    console.log(autocomplete('münchen'))
+    const handleUpdate = (e) => {
+        let value = e.target.value
+        let tmp = []
+        autocomplete(value, 5, true).forEach(item => {
+            tmp.push(names.find(it => {
+                return it.id === item.id
+            }).name)
+        })
+        setSuggestion(tmp)
+        setSearchWord(value)
+    }
 
     return (
-        <>
-
-        </>
+        <div style={}>
+            <div className={"search-inner"}>
+                <input
+                    type={"text"}
+                    name={"Suche Station"}
+                    id={"searchBar"}
+                    placeholder={"Suche Station"}
+                    value={searchWord}
+                    onChange={handleUpdate}
+                />
+                <button name={"Search"}
+                        onClick={() => {
+                        }}
+                >
+                    Search
+                </button>
+            </div>
+            <div className={"dropdown"}>
+                {
+                    (!suggestion.includes(searchWord))
+                    &&
+                    suggestion.map(it =>
+                        <div key={it}
+                             onClick={() => setSearchWord(it)}
+                        >
+                            {it}
+                        </div>
+                    )
+                }
+            </div>
+        </div>
     )
 }
 
